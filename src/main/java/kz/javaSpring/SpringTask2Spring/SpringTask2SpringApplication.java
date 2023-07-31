ddf7b009d9762b3c8d8b9373eb3a692152ed3105
Ak0oNn <133880573+Ak0oNn@users.noreply.github.com>
2023-07-31 18:06:59 +0500

    Add files via upload

---
+++ src/main/java/kz/javaSpring/SpringTask2Spring/SpringTask2SpringApplication.java
@@ -0,0 +1,13 @@
+package kz.javaSpring.SpringTask2Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTask2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTask2SpringApplication.class, args);
+	}
+
+}
